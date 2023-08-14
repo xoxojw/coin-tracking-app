7db53cb0630f98755ab2f57063d36529a7dc9a53
Jiwon Park <xoxojw.dev@gmail.com>
2023-08-14 10:28:32 +0900

    :pencil2: Import font 코드라인 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,34 @@
+import { useState } from "react";
 import { Helmet } from "react-helmet";
+import { ThemeProvider } from 'styled-components';
+import { darkTheme, lightTheme } from './styles/theme';
 import GlobalStyle from "./styles/GlobalStyle";
 import Router from "./routes/Router";
+import styled from "styled-components";
 
 const App = () => {
+  const [isDark, setIsDark] = useState(false);
+  const toggleDark = () => setIsDark(current => !current);
   return (
     <>
-      <Helmet>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap" />
-      </Helmet>
-      <GlobalStyle />
-      <Router />
+      <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+        <GlobalStyle />
+        <ToggleButton onClick={toggleDark}>{isDark ? "🌞" : "🌙"}</ToggleButton>
+        <Router />
+      </ThemeProvider>
     </>
   );
 };
 
 export default App;
+
+const ToggleButton = styled.button`
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  margin: 0.5em;
+  border: none;
+  background-color: transparent;
+  font-size: 24px;
+  cursor: pointer;
+`
