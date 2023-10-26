820d26f21d7a143a556443e25429b08277315531
Jiwon Park <xoxojw.dev@gmail.com>
2023-10-26 21:41:45 +0900

    :lipstick: 차트 선형에서 봉차트 형태로 변경

--- src/libs/helper/comma.ts
+++ src/libs/helper/comma.ts
@@ -1,7 +1,19 @@
 export const thousandsCommaFormatter = (inputNum: number) => {
   let formattedNum = inputNum.toFixed(3); // 소수점 이하의 숫자가 있으면 소수점 3자리까지 나타나도록 형식화
-  formattedNum = formattedNum.replace(/(\.\d*?)0+$/, "$1"); // 불필요한 0 제거
+  formattedNum = formattedNum.replace(/(\.\d*?)0+$/, "$1"); // 불요한 0 제거
   formattedNum = formattedNum.replace(/\.$/, ""); // 소수점 이하 숫자가 없을 경우 소수점 제거
 
   return formattedNum.replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
 }
+
+export const priceFormatter = (num: number) => {
+  // Convert the number to a string and split it into integer and decimal parts
+  const [integerPart, decimalPart] = num.toFixed(1).split('.');
+
+  // Add commas as thousands separators to the integer part
+  const formattedIntegerPart = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+  // Join the formatted integer and decimal parts with a dot
+  return `${formattedIntegerPart}.${decimalPart}`;
+};
+
--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -2,103 +2,98 @@ import { useQuery } from "react-query";
 import { fetchCoinHistory } from "../libs/service/api";
 import ApexChart from "react-apexcharts";
 
-import { thousandsCommaFormatter } from "../libs/helper/comma";
-
 import { useRecoilValue } from "recoil";
 import { isDarkAtom } from "../recoil/atoms";
+
+import { chartTimestamp } from "../libs/helper/date";
+import { priceFormatter } from "../libs/helper/comma";
 
 import * as S from "../styles/Chart.style";
 
 interface ChartProps {
-  coinId: string;
+	coinId: string;
 }
 
 interface IHistoryData {
-  time_open: number;
-  time_close: number;
-  open: string;
-  high: string;
-  low: string;
-  close: string;
-  volume: string;
-  market_cap: number;
+	time_open: number;
+	time_close: number;
+	open: string;
+	high: string;
+	low: string;
+	close: string;
+	volume: string;
+	market_cap: number;
 }
 
 const Chart = ({ coinId }: ChartProps) => {
-  const isDark = useRecoilValue(isDarkAtom);
-  // console.log(coinId);
-  const { isLoading, data } = useQuery<IHistoryData[]>(
-    ["history", coinId],
-    () => fetchCoinHistory(`${coinId}`),
-    {
-      refetchInterval: 10000,
-    }
-  )
-  return (
-    <div>
-      {isLoading ? (
-        "Loading chart..."
-      ) : (
-        <>
-          <S.ChartTitle>Closing Price</S.ChartTitle>
-          <ApexChart
-            type="line"
-            series={[
+	const isDark = useRecoilValue(isDarkAtom);
+	const { isLoading, data } = useQuery<IHistoryData[]>(
+		["history", coinId],
+		() => fetchCoinHistory(`${coinId}`),
+		{
+			refetchInterval: 10000,
+		}
+	);
+	const exceptData = Array.isArray(data) ? data : [];
+	const chartData = exceptData?.map((v) => {
+		return {
+			x: chartTimestamp(v.time_close),
+			y: [v.open, v.high, v.low, v.close].map((i) => priceFormatter(Number(i))),
+		};
+	});
+	console.log(chartData);
+	return (
+		<div>
+			{isLoading ? (
+				"Loading chart..."
+			) : (
+				<>
+					<S.ChartTitle>Closing Price</S.ChartTitle>
+					<ApexChart
+						type="candlestick"
+						series={[
               {
-                name: "price",
-                data: data?.map((price) => Number(price.close)) as number[],
-              },
-            ]}
-            options={{
-              theme: {
-                mode: isDark ? "dark" : "light",
-              },
-              chart: {
-                height: 500,
-                width: 500,
-                background: "transparent",
-                toolbar: {
-                  show: false,
-                },
-              },
-              stroke: {
-                width: 4,
-              },
+								data: chartData,
+							},
+						]}
+						options={{
+							theme: {
+								mode: isDark ? "dark" : "light",
+							},
+							chart: {
+								height: 500,
+								width: 500,
+								background: "transparent",
+								toolbar: {
+									show: false,
+								},
+							},
+							stroke: {
+								width: 4,
+							},
               xaxis: {
                 type: "category",
-                categories: data?.map((date) => {
-                  const time = new Date(date.time_close * 1000)
-                  const day = time.getDate().toString();
-                  const month = (time.getMonth() + 1).toString();
-                  return `${month}/${day}`;
-                }),
-                axisBorder: { show: false },
-                axisTicks: { show: false },
-                labels: { show: false },
-              },
+								axisBorder: { show: false },
+								axisTicks: { show: false },
+								labels: { show: false },
+							},
               yaxis: {
-                axisBorder: { show: false },
-              },
-              fill: {
-                type: "gradient",
-                gradient: {
-                  gradientToColors: ["#60c4c9"],
-                }
-              },
-              colors: ["#9c88ff"],
-              tooltip: {
-                y: {
-                  formatter: (value) => {
-                    return thousandsCommaFormatter(value);
-                  }
-                }
-              }
-            }}
-          />    
-        </>
-      )}
-    </div>
-  );
+								axisBorder: { show: false },
+							},
+							plotOptions: {
+								candlestick: {
+									colors: {
+										upward: "#38c176",
+										downward: "#df5d46",
+									},
+								},
+							},
+						}}
+					/>
+				</>
+			)}
+		</div>
+	);
 };
 
 export default Chart;
